b9b011d999b66a106516eb2d60f0aabbec987268
triszt4n <trisztanpiller@gmail.com>
2021-08-21 23:57:59 +0200

    Search in description too

--- app/javascript/packs/album-index.js
+++ app/javascript/packs/album-index.js
@@ -4,9 +4,12 @@ const albumColumns = document.querySelectorAll("#albumColumn")
 searchBar.addEventListener('keyup', () => {
     const filter = searchBar.value.toUpperCase()
     // Loop through all list items, and hide those who don't match the search query
-    albumColumns.forEach((albumColumn, index) => {
+    albumColumns.forEach((albumColumn) => {
         const title = albumColumn.dataset.title
-        if (title.toUpperCase().indexOf(filter) > -1) {
+        const desc = albumColumn.dataset.desc
+        if (title.toUpperCase().indexOf(filter) > -1 ||
+            desc.toUpperCase().indexOf(filter) > -1 
+        ) {
             albumColumn.style.display = ""
         } else {
             albumColumn.style.display = "none"
