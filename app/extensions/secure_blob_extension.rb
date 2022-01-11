96225c2ce60438a9c6db52cceff25475fb323211
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-01-11 17:31:49 +0100

    Refine authorization code

--- app/extensions/secure_blob_extension.rb
+++ app/extensions/secure_blob_extension.rb
@@ -1,36 +1,28 @@
-ActiveStorage::BaseController.class_eval do
-  before_action :set_album
-  before_action :authorize_request
+# As of rails 6.1.4 these are the classes that handle ActiveStorage serving,
+# so we can set the authorization before_actions there.
+active_storage_controllers = [ActiveStorage::Blobs::RedirectController,
+                              ActiveStorage::Blobs::ProxyController,
+                              ActiveStorage::Representations::BaseController]
 
-  def authorize_request
-    # Edge case, image blob is not in an album, for now it should
-    # send the image, as only album images are restricted
-    return if @album.nil? || @album.public
+active_storage_controllers.each do |controller|
+  controller.class_eval do
+    before_action :authorize_request
+    def authorize_request
+      album = Album.find_blob_owner @blob.id
+      # If resource is not from an album, or album is public, then no need to authorize.
+      return if album.nil? || album.public
 
-    user = current_user
+      # if album is not public, and user is logged in check permissions
+      user = User.find_by(id: session[:user_id])
+      unless user.present? && (user.site_admin? || logged_in_as_admin_of?(album.circle) || album.shared?)
+        redirect_to '/', notice: 'Az erőforrás eléréséhez be kell jelentkezned'
+      end
+    end
 
-    # if album is not public, require login
-    return head :forbidden if user.nil?
-
-    # if album is not public, and user is logged in check permissions
-    head :forbidden unless user.site_admin? || logged_in_as_admin_of?(@album.circle) || @album.shared?
-  end
-
-  # Can't use helpers defined in application_controller because
-  # ActiveStorage::BaseController is not a child of it.
-  def current_user
-    User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def set_album
-    blob     = ActiveStorage::Blob.find_signed(params[:signed_blob_id] || params[:signed_id])
-    album_id = blob&.attachments&.where(record_type: Album.name)&.first&.record_id
-    @album   = Album.find album_id
-  rescue ActiveRecord::RecordNotFound
-    @album = nil
-  end
-
-  def logged_in_as_admin_of?(circle)
-    Membership.exists?(user: current_user, circle: circle, admin: true)
+    # Can't use helpers defined in application_controller because
+    # ActiveStorage::BaseController is not a child of it.
+    def logged_in_as_admin_of?(circle)
+      Membership.exists?(user: current_user, circle: circle, admin: true)
+    end
   end
 end
