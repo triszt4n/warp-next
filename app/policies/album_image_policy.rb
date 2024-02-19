c84a4a1629da47793dc2563bae97f3e5040eff16
Bucsy Benjamin <bucsy.benjamin@simonyi.bme.hu>
2024-02-19 23:57:42 +0100

    chore: add policy for album images

---
+++ app/policies/album_image_policy.rb
@@ -0,0 +1,25 @@
+class AlbumImagePolicy < ApplicationPolicy
+
+  def show?
+    return true if @record.album.public?
+
+    # if not public, lets check for proper access
+    site_admin? || circle_admin? || (circle_member? && @record.album.shared?)
+  end
+
+
+  private
+
+  def site_admin?
+    @user.present? && @user.site_admin?
+  end
+
+  def circle_admin?
+    @user.present? && Membership.exists?(user: @user, circle: @record.album.circle, admin: true)
+  end
+
+  def circle_member?
+    @user.present? && Membership.exists?(user: @user, circle: @record.album.circle, accepted: true)
+  end
+
+end
